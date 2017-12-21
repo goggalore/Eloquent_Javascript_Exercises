62b53feac469c051e651e1b5c67d45fa1ab8cd92
goggalore <junec177@gmail.com>
2017-12-21 15:27:12 -0700

    updated solutions

--- 6. The Secret Life of Objects/sequence.js
+++ 6. The Secret Life of Objects/sequence.js
@@ -3,33 +3,54 @@ function ArraySeq(array) {
   this.array = array;
 }
 ArraySeq.prototype.next = function() {
-  if (this.position >= this.array.length - 1)
-    return false;
-  this.position++;
+  if (this.position >= this.array.length - 1) {
+    return false
+  }
+  
+  this.position++
   return true;
-};
+}
 ArraySeq.prototype.current = function() {
   return this.array[this.position];
-};
+}
 
-function RangeSeq(from, to) {
+
+function RangeSeq(from, to) { 
   this.position = from - 1;
   this.to = to;
 }
 RangeSeq.prototype.next = function() {
-  if (this.position >= this.to)
-    return false;
-  this.position++;
-  return true;
-};
+  if (this.position >= this.to) {
+    return false 
+  }
+  
+  this.position++
+  return true
+}
 RangeSeq.prototype.current = function() {
   return this.position;
-};
+}
 
 function logFive(sequence) {
   for (var i = 0; i < 5; i++) {
-    if (!sequence.next())
+    if(!sequence.next()) {
       break;
+    }
+    
+    console.log(sequence.current());
+  }
+}
+
+function logNTimes(sequence, n) {
+  if (n === undefined){
+    n = 1;
+  }
+  
+  for (var i = 0; i < n; i++) {
+    if (!sequence.next()) {
+      break;
+    }
+    
     console.log(sequence.current());
   }
 }
@@ -43,3 +64,14 @@ logFive(new RangeSeq(100, 1000));
 // → 102
 // → 103
 // → 104
+logNTimes(new RangeSeq(100, 1000), 10)
+// → 100
+// → 101
+// → 102
+// → 103
+// → 104
+// → 105
+// → 106
+// → 107
+// → 108
+// → 109
