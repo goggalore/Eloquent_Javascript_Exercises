a3b13dedefa7cc985a62f69308945dc1d889994e
goggalore <junec177@gmail.com>
2017-12-25 16:33:45 -0700

    updated solutions

--- 6. The Secret Life of Objects/vector.js
+++ 6. The Secret Life of Objects/vector.js
@@ -5,7 +5,7 @@ function Vector(x, y) {
 
 Object.defineProperty(Vector.prototype, 'norm', {
   get: function() {
-  return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+    return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
   }
 });
 
@@ -17,11 +17,17 @@ Vector.prototype.minus = function(vector) {
   return new Vector(this.x - vector.x, this.y - vector.y);
 }
 
+Vector.prototype.distance = function(vector) {
+  return Math.sqrt(Math.pow((this.x - vector.x), 2) + Math.pow((this.y - vector.y), 2));
+}
+
 console.log(new Vector(1, 2).plus(new Vector(2, 3)));
 // → Vector{x: 3, y: 5}
 console.log(new Vector(1, 2).minus(new Vector(2, 3)));
 // → Vector{x: -1, y: -1}
 console.log(new Vector(3, 4).norm);
+// → 5
+console.log(new Vector(2, 2).distance(new Vector(6, 5)));
 // → 5
 
 module.exports = vector;
